b903eeb6e099b465378d3a823273d0479cbf7f2c
Pragyanshu Pushker <pragyanshupushker@gmail.com>
2025-02-09 16:15:51 +0530

    updating necessary env vars

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,91 +1,85 @@
-import React, { useContext, useState } from "react"
-import axios from 'axios'
+import React, { useContext, useState } from "react";
+import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const BASE_URL = "https://expense-tracker-nine-alpha-43.vercel.app/api/v1/";
+const GlobalContext = React.createContext();
 
-
-const GlobalContext = React.createContext()
-
-export const GlobalProvider = ({children}) => {
-
-    const [incomes, setIncomes] = useState([])
-    const [expenses, setExpenses] = useState([])
-    const [error, setError] = useState(null)
+export const GlobalProvider = ({ children }) => {
+    const [incomes, setIncomes] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+    const [error, setError] = useState(null);
 
     //calculate incomes
     const addIncome = async (income) => {
         const response = await axios.post(`${BASE_URL}add-income`, income)
-            .catch((err) =>{
-                setError(err.response.data.message)
-            })
-        getIncomes()
-    }
+            .catch((err) => {
+                setError(err.response.data.message);
+            });
+        getIncomes();
+    };
 
     const getIncomes = async () => {
-        const response = await axios.get(`${BASE_URL}get-income`)
-        setIncomes(response.data)
-        console.log(response.data)
-    }
+        const response = await axios.get(`${BASE_URL}get-income`);
+        setIncomes(response.data);
+        console.log(response.data);
+    };
 
     const deleteIncome = async (id) => {
-        const res  = await axios.delete(`${BASE_URL}delete-income/${id}`)
-        getIncomes()
-    }
+        const res = await axios.delete(`${BASE_URL}delete-income/${id}`);
+        getIncomes();
+    };
 
     const totalIncome = () => {
         let totalIncome = 0;
-        incomes.forEach((income) =>{
-            totalIncome = totalIncome + income.amount
-        })
+        incomes.forEach((income) => {
+            totalIncome = totalIncome + income.amount;
+        });
 
         return totalIncome;
-    }
-
+    };
 
     //calculate incomes
     const addExpense = async (income) => {
         const response = await axios.post(`${BASE_URL}add-expenses`, income)
-            .catch((err) =>{
-                setError(err.response.data.message)
-            })
-        getExpenses()
-    }
+            .catch((err) => {
+                setError(err.response.data.message);
+            });
+        getExpenses();
+    };
 
     const getExpenses = async () => {
-        const response = await axios.get(`${BASE_URL}get-expenses`)
-        setExpenses(response.data)
-        console.log(response.data)
-    }
+        const response = await axios.get(`${BASE_URL}get-expenses`);
+        setExpenses(response.data);
+        console.log(response.data);
+    };
 
     const deleteExpense = async (id) => {
-        const res  = await axios.delete(`${BASE_URL}delete-expense/${id}`)
-        getExpenses()
-    }
+        const res = await axios.delete(`${BASE_URL}delete-expense/${id}`);
+        getExpenses();
+    };
 
     const totalExpenses = () => {
         let totalIncome = 0;
-        expenses.forEach((income) =>{
-            totalIncome = totalIncome + income.amount
-        })
+        expenses.forEach((income) => {
+            totalIncome = totalIncome + income.amount;
+        });
 
         return totalIncome;
-    }
-
+    };
 
     const totalBalance = () => {
-        return totalIncome() - totalExpenses()
-    }
+        return totalIncome() - totalExpenses();
+    };
 
     const transactionHistory = () => {
-        const history = [...incomes, ...expenses]
+        const history = [...incomes, ...expenses];
         history.sort((a, b) => {
-            return new Date(b.createdAt) - new Date(a.createdAt)
-        })
+            return new Date(b.createdAt) - new Date(a.createdAt);
+        });
 
-        return history.slice(0, 5)
-    }
-
+        return history.slice(0, 5);
+    };
 
     return (
         <GlobalContext.Provider value={{
@@ -106,9 +100,9 @@ export const GlobalProvider = ({children}) => {
         }}>
             {children}
         </GlobalContext.Provider>
-    )
-}
+    );
+};
 
-export const useGlobalContext = () =>{
-    return useContext(GlobalContext)
-}
+export const useGlobalContext = () => {
+    return useContext(GlobalContext);
+};
